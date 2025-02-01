ab4c492e750135908f45e3583c78500abe6271a9
Metin Koyuncu <130415130+metinkoyuncuu@users.noreply.github.com>
2025-02-02 01:08:51 +0300

    .

---
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
+using SignalR.EntiyLayer.Entities;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SignalR.DataAccessLayer.Concrete
+{
+    public class SignalRContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;initial Catalog=SignalRDb;integrated Security=true");
+        }
+
+        //Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = SignalRDb; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<MoneyCase> MoneyCases { get; set; }
+        public DbSet<MenuTable> MenuTables { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+
+
+    }
+}
